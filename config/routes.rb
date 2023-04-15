ed6d85e79c82b054f8b5e01466c256b1cd90ebc1
ricky zenati <b00629577@essec.edu>
2023-04-15 09:00:04 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   get 'love_messages/new'
   get 'love_messages/create'
   get 'love_messages/show'
   get 'love_messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
   resources :love_messages
   # Defines the root path route ("/")
   # root "articles#index"
